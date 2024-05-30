64d92888647d54b120c787036cbaea74ddf40e50
Debasis08 <patrodebasis8@gmail.com>
2024-05-30 14:30:48 +0530

    Third Commit

--- server/index.js
+++ server/index.js
@@ -17,4 +17,21 @@ io.on("connection", (socket) => {
         socket.join(room);
         io.to(socket.id).emit("join-room", data);
     });
+
+    socket.on('user:call', ({ to, offer}) => {
+        io.to(to).emit('incoming:call', { from: socket.id, offer});
+    });
+
+    socket.on('call-accepted', ({to, ans}) => {
+        io.to(to).emit('call-accepted', {from: socket.id, ans})
+    });
+
+    socket.on('peer:nego:needed', ({ to, offer}) => {
+        io.to(to).emit('peer:nego:needed', {from: socket.id, offer})
+    });
+    
+    socket.on('peer:nego:done', ({ to, ans}) => {
+        io.to(to).emit('peer:nego:final', {from: socket.id, ans})
+    });
+
 });
